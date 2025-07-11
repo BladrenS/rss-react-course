8a7d80a1f02e2afc119505baa2dad40d10d6c188
[BladrenS] <denis_ka_98@mail.ru>
2025-07-12 00:05:35 +0600

    feat: add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="p-4 border-b">
+        <h1 className="text-2xl font-bold">Pokemon Search</h1>
+      </div>
+    );
+  }
+}
