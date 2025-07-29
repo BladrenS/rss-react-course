5331dbc809b1654c7edee9133b7cccf411b7e140
[BladrenS] <denis_ka_98@mail.ru>
2025-07-30 00:30:12 +0600

    feat: add some animations

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { Card } from './Card';
 import { PokemonData } from '../types/types';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence, Variants, Transition } from 'framer-motion';
 
 interface CardListProps {
   items: PokemonData[];
@@ -20,22 +20,65 @@ export const CardList: FC<CardListProps> = ({
     );
   }
 
+  const containerVariants: Variants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        when: 'beforeChildren',
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const itemTransition: Transition = {
+    type: 'spring',
+    stiffness: 100,
+    damping: 10,
+  };
+
+  const itemVariants: Variants = {
+    hidden: {
+      opacity: 0,
+      x: -50,
+      y: 20,
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      y: 0,
+      transition: itemTransition,
+    },
+  };
+
   return (
     <motion.div
       animate={{ marginRight: isDetailsVisible ? '25%' : 0 }}
       transition={{ duration: 0.3 }}
     >
-      <div className="p-4 max-w-2xl mx-auto flex flex-wrap justify-around">
-        {items.map((pokemon) => (
-          <div
-            key={pokemon.name}
-            onClick={() => onItemClick(pokemon.name)}
-            className="cursor-pointer"
-          >
-            <Card {...pokemon} />
-          </div>
-        ))}
-      </div>
+      <motion.div
+        className="p-4 max-w-2xl mx-auto flex flex-wrap justify-around"
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        <AnimatePresence>
+          {items.map((pokemon) => (
+            <motion.div
+              key={pokemon.name}
+              onClick={() => onItemClick(pokemon.name)}
+              className="cursor-pointer m-2"
+              variants={itemVariants}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              exit={{ opacity: 0, x: -50 }}
+              layout
+            >
+              <Card {...pokemon} />
+            </motion.div>
+          ))}
+        </AnimatePresence>
+      </motion.div>
     </motion.div>
   );
 };
